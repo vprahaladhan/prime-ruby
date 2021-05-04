921d6f53efca13a5dbae88b2cb0bcea0119a9108
prahal <vprahaladhan@gmail.com>
2021-05-04 20:56:46 +1000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  divider = 2
+  prime_no = true
+
+  if number <= 1 then
+    prime_no = false
+  else 
+    until divider == number do
+      if number.modulo(divider) == 0 then
+        prime_no = false
+        divider += 1
+        break
+      end
+    end
+  end
+
+  prime_no
+
+end
